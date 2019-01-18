3f42d77b1b4508da0cfba35d82d6dcb4d752ee65
RobQuistNL <rob.quist@dukesoft.nl>
2019-01-18 13:44:02 +0100

    Working color part

--- objects/obj_test/Create_0.gml
+++ objects/obj_test/Create_0.gml
@@ -1,4 +1,52 @@
 dll_makebuffer = external_define("test.dll", "makebuffer", dll_cdecl, ty_real, 1, ty_string);
 
-buffer = buffer_create(3, buffer_fast, 1);
-external_call(dll_makebuffer, buffer_get_address(buffer));
+vbuf_size = 0;
+vertex_format_begin();
+vertex_format_add_color(); vbuf_size += 4; //4 bytes (RGBA)
+vbuf_format = vertex_format_end();
+
+vbuf = vertex_create_buffer();
+vertex_begin(vbuf, vbuf_format);
+vertex_color(vbuf, c_red, 0.5);
+vertex_color(vbuf, c_lime, 0.5);
+vertex_color(vbuf, c_blue, 0.5);
+vertex_color(vbuf, c_black, 0);
+vertex_color(vbuf, c_white, 1);
+vertex_end(vbuf);
+
+show_debug_message("vbuf size: " + string(vertex_get_buffer_size(vbuf)) + " (calc: "+string(vbuf_size)+")");
+
+buffer_from_vbuffer = buffer_create_from_vertex_buffer(vbuf, buffer_fast, 1);
+show_debug_message("actual buffer size: " + string(buffer_get_size(buffer_from_vbuffer)));
+
+dll_buffer = buffer_create(vbuf_size*5, buffer_fast, 1);
+external_call(dll_makebuffer, buffer_get_address(dll_buffer));
+
+
+for (var i = 0; i < buffer_get_size(buffer_from_vbuffer); i++) {
+	var gmbuf = buffer_read(buffer_from_vbuffer, buffer_u8);
+	var dllbuf = buffer_read(dll_buffer, buffer_u8);
+	var extrstr = "";
+	if (gmbuf != dllbuf) {
+		extrstr = "MISMATCH";
+	}
+	show_debug_message("A: " + string(gmbuf) + "\t/ " + string(dllbuf) + "\t" + extrstr);	
+	//show_debug_message("D: " + string(buffer_read(buffer_from_vbuffer, buffer_u8)));	
+}
+
+
+//for (var i = 0; i < buffer_get_size(dll_buffer); i++) {
+//	show_debug_message("D: " + );	
+//}
+
+//show_debug_message("COPI vbuf size: " + string(vertex_get_buffer_size(vbuf)));
+
+/*
+vertex_format_begin();
+vertex_format_add_position_3d();
+vbuf_format = vertex_format_end();
+
+vbuf = vertex_create_buffer();
+vertex_begin(vbuf, vbuf_format);
+vertex_position_3d(vbuf, 10, 30, 50.5);
+vertex_end(vbuf);
--- src/test.cpp
+++ src/test.cpp
@@ -47,6 +47,37 @@ vertex_triangle(
 			);
 */
 
+class color {
+    public:
+    unsigned char r, g, b, a;
+
+    color()
+    {
+        r = g = b = a =0;
+    }
+
+    color(float f_r, float f_g, float f_b, float f_a)
+    {
+        r = f_r * 255;
+        g = f_g * 255;
+        b = f_b * 255;
+        a = f_a * 255;
+    }
+/*
+    color(unsigned char f_r, unsigned char f_g, unsigned char f_b, unsigned char f_a)
+    {
+        r = f_r;
+        g = f_g;
+        b = f_b;
+        a = f_a;
+    }
+*/
+    white()
+    {
+        r = g = b = a = 255;
+    }
+};
+
 int packFloat(void *buf, float x) {
     unsigned char *b = (unsigned char *)buf;
     unsigned char *p = (unsigned char *) &x;
@@ -62,6 +93,11 @@ int packFloat(void *buf, float x) {
     b[3] = p[3];
 #endif
     return 4;
+}
+
+int writeByte(char* vbuf, int cursor, unsigned char value) {
+    vbuf[cursor] = value; cursor ++;
+    return cursor;
 }
 
 int writeLong(char* vbuf, int cursor, long value) {
@@ -99,8 +135,11 @@ int vertex_normal(char* vbuf, int cursor, float x, float y, float z) {
     return cursor;
 }
 
-int vertex_color(char* vbuf, int cursor, int color) {
-    cursor = writeLong(vbuf, cursor, color);
+int vertex_color(char* vbuf, int cursor, color color) {
+    cursor = writeByte(vbuf, cursor, color.r);
+    cursor = writeByte(vbuf, cursor, color.g);
+    cursor = writeByte(vbuf, cursor, color.b);
+    cursor = writeByte(vbuf, cursor, color.a);
     return cursor;
 }
 
@@ -109,38 +148,49 @@ int vertex_texcoord(char* vbuf, int cursor, float u, float v) {
     cursor = writeFloat(vbuf, cursor, v);
     return cursor;
 }
-
+/*
 int vertex_triangle(char* vbuf, int cursor, float x1, float y1, float z1, float x2, float y2, float z2,  float x3, float y3, float z3, float u0, float v0, float u1, float v1, float u2, float v2, int color) {
     unsigned char found[4];
 
     cursor = vertex_position_3d(vbuf, cursor, x1, y1, z1);
     cursor = vertex_normal(vbuf, cursor, found[2], found[3], found[4]);
-    cursor = vertex_color(vbuf, cursor, color);
+    cursor = vertex_color(vbuf, cursor, color::color());
     cursor = vertex_texcoord(vbuf, cursor, u0, v0);
-    cursor = vertex_color(vbuf, cursor, found[0]);
+    cursor = vertex_color(vbuf, cursor, (new color).white());
 
     cursor = vertex_position_3d(vbuf, cursor, x2, y2, z2);
     cursor = vertex_normal(vbuf, cursor, found[2], found[3], found[4]);
-    cursor = vertex_color(vbuf, cursor, color);
+    cursor = vertex_color(vbuf, cursor, color::white());
     cursor = vertex_texcoord(vbuf, cursor, u1, v1);
-    cursor = vertex_color(vbuf, cursor, found[0]);
+    cursor = vertex_color(vbuf, cursor, color::white());
 
     cursor = vertex_position_3d(vbuf, cursor, x3, y3, z3);
     cursor = vertex_normal(vbuf, cursor, found[2], found[3], found[4]);
-    cursor = vertex_color(vbuf, cursor, color);
+    cursor = vertex_color(vbuf, cursor, color::white());
     cursor = vertex_texcoord(vbuf, cursor, u2, v2);
-    cursor = vertex_color(vbuf, cursor, found[0]);
+    cursor = vertex_color(vbuf, cursor, color::white());
 
     return cursor;
 }
+*/
 
 extern "C" {
 
     double chunkSize, ter_octaves, ter_persistance, ter_scale, temp_octaves, temp_persistance, temp_scale, moist_octaves, moist_persistance, moist_scale;
 
     double makebuffer(char* buffer) {
-        buffer[0] = 123;
+        int cursor = 0;
+        color _red(1, 0, 0, .5);
+        color _green(0, 1, 0, .5);
+        color _blue(0, 0, 1, .5);
+        color _white(1, 1, 1, 1);
+        color _black(0, 0, 0, 0);
 
+        cursor = vertex_color(buffer, cursor, _red);
+        cursor = vertex_color(buffer, cursor, _green);
+        cursor = vertex_color(buffer, cursor, _blue);
+        cursor = vertex_color(buffer, cursor, _black);
+        cursor = vertex_color(buffer, cursor, _white);
         return 1;
     }
 
@@ -152,8 +202,8 @@ extern "C" {
                 //int noiseX = xx*lod + (chunkX*chunkSize) + 1000;
                 //int noiseY = yy*lod + (chunkY*chunkSize) + 4000;
                 float s = 32;
-                c = vertex_triangle(vbuf, c, xx*s, yy*s, 10, (xx+1)*s, yy*s, 10, xx*s, (yy+1)*s, 10, 0, 0, 1, 1, 1, 1, 255);
-                c = vertex_triangle(vbuf, c, xx*s, (yy+1)*s, 10, (xx+1)*s, yy*s, 10, (xx+1)*s, (yy+1)*s, 10, 0, 0, 1, 1, 1, 1, 255);
+                //c = vertex_triangle(vbuf, c, xx*s, yy*s, 10, (xx+1)*s, yy*s, 10, xx*s, (yy+1)*s, 10, 0, 0, 1, 1, 1, 1, 255);
+                //c = vertex_triangle(vbuf, c, xx*s, (yy+1)*s, 10, (xx+1)*s, yy*s, 10, (xx+1)*s, (yy+1)*s, 10, 0, 0, 1, 1, 1, 1, 255);
 
                 /*
                 vertex_triangle(
